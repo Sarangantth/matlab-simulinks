0c0ceae85d24c4e59d365a8ff960152a5482226e
Sarangantth <sarangantth@gmail.com>
2024-01-13 17:25:58 +0530

    test commit of demo java program in main.

---
+++ demo.java
@@ -0,0 +1,25 @@
+// import java.util.Arrays;
+
+class demo{
+    public static void main(String a[])
+    {
+        int num[][] = new int[3][];      //jagged array
+
+        num[0]= new int[3];
+        num[1]= new int[4];
+        num[2]= new int[2];
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<num[i].length;j++){
+                num[i][j] = (int)(Math.random()*10);
+            }
+        }
+
+        for(int n[] : num){
+            for(int m : n){
+                System.out.print( m + " ");
+            }
+            System.out.println();
+        }
+    }
+}
